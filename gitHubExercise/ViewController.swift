1b34d26d877604da858bdf48958ca91d11231154
BrettForeman <bfcollege7@yahoo.com>
2016-12-29 23:55:23 -0700

    Third commit for this project

--- gitHubExercise/ViewController.swift
+++ gitHubExercise/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         "That's how you Fuckin play"
+        "This should show up"
+        
     }
 
     override func didReceiveMemoryWarning() {
