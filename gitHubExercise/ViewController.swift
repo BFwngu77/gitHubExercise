8e36b8196f7bddbedfafb1ffed9c16b614203e74
BrettForeman <bfcollege7@yahoo.com>
2016-12-30 00:00:22 -0700

    This is the fourth change

--- gitHubExercise/ViewController.swift
+++ gitHubExercise/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         "That's how you Fuckin play"
         "This should show up"
+        "What is this"
         
     }
 
